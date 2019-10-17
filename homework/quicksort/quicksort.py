cd0f06c0a1b8d6d2c64ac3bc02152f217b10a75e
sean <55726331+linseanwin@users.noreply.github.com>
2019-10-18 00:21:38 +0800

    Update quicksort.py

--- homework/quicksort/quicksort.py
+++ homework/quicksort/quicksort.py
@@ -1,1 +1,20 @@
+numbers = [98, 39, 88, 11, 29, 85, 21, 6, 36] 
 
+def quick_sort(numbers): 
+    if len(numbers) <= 1:
+        return numbers
+    
+    left= [] 
+    right = [] 
+    mid = []    
+    pivot = numbers[0] 
+
+    for number in numbers:
+        if number == pivot:
+            mid.append(number)
+        elif number < pivot:
+            left.append(number)
+        else:
+            right.append(number) 
+    return quick_sort(left) + mid + quick_sort(right)
+  print(quick_sort(numbers))
