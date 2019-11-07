ec64fe72d6d4ccc57d7ba4220bfe1ca34b283114
sean <55726331+linseanwin@users.noreply.github.com>
2019-11-08 03:15:22 +0800

    Update merge_sort_06170140.py

--- homework/hw2/merge_sort_06170140.py
+++ homework/hw2/merge_sort_06170140.py
@@ -1,1 +1,25 @@
+class Solution(object):
+    
+    def merge(self,left,right):
+        ans = []
+        while len(left) > 0 or len(right) > 0:        
+            if len(left) > 0 and len(right) > 0:     
+                if left[0] <= right[0]:              
+                    ans.append(left.pop(0))       
+                else:
+                    ans.append(right.pop(0))      
+            elif len(left) > 0:                     
+                ans.append(left.pop(0))
+            elif len(right) > 0:
+                ans.append(right.pop(0))
+        return ans
 
+def merge_sort(self,nums):
+        if len(nums) <= 1:
+            return nums
+        else:
+            number = len(nums)//2               
+            left = Solution().merge_sort(list[:number])     
+            right = Solution().merge_sort(list[number:])
+            
+            return Solution().merge(left, right)
