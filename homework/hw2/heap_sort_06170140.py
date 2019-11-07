bb11a5b8123edac27d6146bacbe5390e45c93ab3
sean <55726331+linseanwin@users.noreply.github.com>
2019-11-07 15:17:25 +0800

    Update heap_sort_06170140.py

--- homework/hw2/heap_sort_06170140.py
+++ homework/hw2/heap_sort_06170140.py
@@ -1,16 +1,16 @@
-def heapify(tree,n,i):
+def heapify(tree,n,i):#寫一顆樹 n代表節點 i代表對該值做heapify
     if i >= n:
         return
-    c1 = 2*i+1
-    c2 = 2*i+2
+    c1 = 2*i+1 #第一個子節點
+    c2 = 2*i+2 #第二個子節點
     maximum = i
         
-    if c1 < n and tree[c1] > tree[i]:
+    if c1 < n and tree[c1] > tree[i]: #c1 c2不可出界 故2,3行補return
             maximum = c1
     if c2 < n and tree[c2] > tree[i]:
             maximum = c2
             
-    if maximum != i: 
+    if maximum != i: #如果i為最大值就不必交換
         tree[i],tree[maximum] = tree[maximum],tree[i]
             
         heapify(tree,n,maximum)
