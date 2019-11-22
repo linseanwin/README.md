e18332ad544d977d5bc60e8359c65d43eb90a733
sean <55726331+linseanwin@users.noreply.github.com>
2019-11-22 23:59:03 +0800

    Update binary_search_tree_06170140.py

--- HW3/binary_search_tree_06170140.py
+++ HW3/binary_search_tree_06170140.py
@@ -5,16 +5,26 @@ class TreeNode(object):
         self.right = None
 class Solution(object):
     def insert(self, root, val):
-        if val <= root.val and root.left is None:
-            root.left = TreeNode(val)
-            return root.left
-        if val > root.val and root.right is None:
-            root.right = TreeNode(val)
-            return root.right
-        if val <= root.val and root.left:
-            root = root.left
-        if val> root.val and root.right:
-            root = root.right
+        
+        NewNode = TreeNode(val)
+        if root:
+            if val <= root.val:
+                if root.left:
+                    return Solution.insert(self, root.left, val)
+                else:
+                    node = TreeNode(val)
+                    root.left = node
+                    
+            else:
+                if root.right:
+                    return Solution.insert(self, root.right, val)
+                else:
+                    node = TreeNode(val)
+                    root.right = node
+                    return root.right
+        else:
+            root=TreeNode(val)
+            return root
             
     def search(self, root, val):
         if root is None:
@@ -32,5 +42,5 @@ class Solution(object):
       
 #參考資料:https://www.geeksforgeeks.org/binary-search-tree-set-1-search-and-insertion/
         #https://zh.wikipedia.org/wiki/%E4%BA%8C%E5%85%83%E6%90%9C%E5%B0%8B%E6%A8%B9
-        
-        #AB班許多王牌高手
+        #https://github.com/C-WeiYu/WeiYu/blob/master/HW3/binary_search_tree_06170201.py
+        #https://github.com/samuel871211/My-python-code/blob/master/HW3/binary_search_tree_06170225.py
