5cad7bfebc90526f334fff86cdd1bf1c035a8093
sean <55726331+linseanwin@users.noreply.github.com>
2019-12-11 16:48:57 +0800

    Create BFS_06170140.py

---
+++ HW5/BFS_06170140.py
@@ -0,0 +1,25 @@
+from collections import defaultdict 
+  
+# This class represents a directed graph 
+# using adjacency list representation 
+class Graph:
+    # Constructor 
+    def __init__(self): 
+        # default dictionary to store graph 
+        self.graph = defaultdict(list) 
+
+    # function to add an edge to graph 
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+  
+    # Function to print a BFS of graph 
+    def BFS(self, s): 
+        """
+        :type s: int
+        :rtype: list
+        """
+    def DFS(self, s):
+        """
+        :type s: int
+        :rtype: list
+        """
