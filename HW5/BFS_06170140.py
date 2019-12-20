c65774c4a4767e7b778f63bfbf4105f67dad9cf2
sean <55726331+linseanwin@users.noreply.github.com>
2019-12-20 23:16:39 +0800

    Update BFS_06170140.py

--- HW5/BFS_06170140.py
+++ HW5/BFS_06170140.py
@@ -41,3 +41,13 @@ class Graph:
                         b.append(c)
                     
         return a
+    
+#參考資料:
+
+#https://alrightchiu.github.io/SecondRound/graph-breadth-first-searchbfsguang-du-you-xian-sou-xun.html
+
+#https://github.com/C-WeiYu/WeiYu/blob/master/HW5/BFS%E8%88%87DFS%E6%B5%81%E7%A8%8B%E5%9C%96%E3%80%81%E7%A8%8B%E5%BC%8F%E7%A2%BC%E5%AD%B8%E7%BF%92%E6%AD%B7%E7%A8%8B%E8%88%87BFS%E8%88%87DFS%E5%8E%9F%E7%90%86%E8%88%87%E6%AF%94%E8%BC%83.ipynb
+
+#https://en.wikipedia.org/wiki/Breadth-first_search
+    
+  
