9ca9f9481f2d323680dfccf392d5f81e8499cfaf
sean <55726331+linseanwin@users.noreply.github.com>
2020-01-03 23:51:29 +0800

    Update Dijkstra_06170140.py

--- HW6/Dijkstra_06170140.py
+++ HW6/Dijkstra_06170140.py
@@ -1,6 +1,4 @@
 from collections import defaultdict 
-
-#Class to represent a graph 
 class Graph(): 
 
     def __init__(self, vertices): 
@@ -8,17 +6,36 @@ class Graph():
         self.graph = [] 
         self.graph_matrix = [[0 for column in range(vertices)]  
                     for row in range(vertices)] 
+        self.dict = defaultdict(list) 
     def addEdge(self,u,v,w): 
-        """
-        :type u,v,w: int
-        :rtype: None
-        """
+        self.dict[w].append([u,v])
     def Dijkstra(self, s): 
-        """
-        :type s: int
-        :rtype: dict
-        """
-    def Kruskal(self):
-        """
-        :rtype: dict
-        """
+        a = [False] * self.V 
+        b = [True] * self.V
+        c = [float('inf')] * self.V
+        
+        
+        c[s] = 0
+        
+        while a != b:
+            for i in range(self.V):
+                if g.graph[i][s] != 0 and c[s] + g.graph[i][s] < c[i]:
+                    c[i] = c[s] + g.graph[i][s]
+                           
+            d = float('inf')
+            e = 0
+            for e in range(self.V):
+                if a[e] == False and c[e] < d :
+                    d = c[e]
+                    s = e         
+            a[s] = True
+            
+            f = {} 
+            if a == b:
+                for i in range(self.V):
+                    f[str(i)] = c[i]
+                return f
+        return 
+    
+     def Kruskal(self):
+         pass   
